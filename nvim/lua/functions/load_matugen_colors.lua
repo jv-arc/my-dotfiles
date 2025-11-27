9548f1d2b764959d442d388dd01ed8f8cbb336c2
jv-arc <jvarc.gm@gmail.com>
2025-11-27 14:10:04 -0300

    loading script for matugen colors in nvim

--- nvim/lua/functions/load_matugen_colors.lua
+++ nvim/lua/functions/load_matugen_colors.lua
@@ -1,34 +1,67 @@
-
 local h2c = require("functions.hextochroma")
 
-local function load_colorscheme()
+local function load_colorscheme(mode)
 	package.loaded["mini.base16"] = nil
 	package.loaded["assets.matugen_colors"] = nil
 	collectgarbage("collect")
 
-	local mode = "mini_palette"
+	if(mode == nil) then
+		mode = "min"
+	end
+
 	local fpalette = require("assets.matugen_colors")
 
-	if mode == "mini_palette" then
+	if mode == "min" then
 		require("mini.base16").setup({
-			palette = require("mini.base16").mini_palette(fpalette.surface, fpalette.on_surface, h2c(fpalette.primary)),
+			palette = require("mini.base16").mini_palette(
+				fpalette.surface,
+				fpalette.on_surface,
+				h2c(fpalette.primary)
+			),
 			plugins = { default = true },
 		})
-	elseif mode == "palette" then
-		require("mini.base16").setup({
-			palette = fpalette,
-			plugins = { default = true },
-		})
-	end
-	vim.defer_fn(function()
-		-- Make background equal to kitty's:
-		vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-		vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 
-		-- Make Number column transparent as well
-		vim.api.nvim_set_hl(0, "LineNrAbove", { bg = "none" })
-		vim.api.nvim_set_hl(0, "LineNrBelow", { bg = "none" })
-	end, 20)
+	elseif mode == "max" then
+		require("mini.base16").setup({
+			base00 = fpalette.background,
+			base01 = fpalette.surface_container_lowest,
+			base02 = fpalette.surface_containert_low,
+			base03 = fpalette.outline_variant,
+			base04 = fpalette.on_surface_variant,
+			base05 = fpalette.on_surface,
+			base06 = fpalette.inverse_on_surface,
+			base07 = fpalette.surface_bright,
+			base08 = fpalette.error,
+			base09 = fpalette.tertiary,
+			base0A = fpalette.secondary,
+			base0B = fpalette.primary,
+			base0C = fpalette.tertiary_container,
+			base0D = fpalette.primary_container,
+			base0E = fpalette.secondary_container,
+			base0F = fpalette.error_container
+		})
+
+	end
+
+	-- Make background equal to kitty's:
+	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+
+	-- Make Number column transparent as well
+	vim.api.nvim_set_hl(0, "LineNrAbove", { bg = "none" })
+	vim.api.nvim_set_hl(0, "LineNrBelow", { bg = "none" })
+
+	-- Make Highlights more visible
+	vim.api.nvim_set_hl(0, 'Visual', {
+		bg = fpalette.primary_container,
+		fg = fpalette.background
+	})
+
+	-- Update lualine separetedely
+	require('lualine').setup({
+		options = { theme = 'base16' }
+	})
+
 end
 
 return load_colorscheme
